6eab07ff403da9211cc4f768d81e1b21adf2b6b9
Andrew Lane <AndrewLane@users.noreply.github.com>
2023-10-15 04:27:21 -0400

    Demonstrate that the API allows for either password or passphrase (#203)
    * Add a test showing you can encrypt using either password or passphrase

--- test/index.spec.js
+++ test/index.spec.js
@@ -18,8 +18,20 @@ describe("basic tests", () => {
     });
   });
 
-  it("encrypts", (done) => {
+  it("encrypts using password", (done) => {
     keygen({ password: "blahblahblah" }, (err, result) => {
+      expect(expect(err).to.be.null);
+      expect(result.private).to.match(/^-----BEGIN RSA PRIVATE KEY-----\n/);
+      expect(result.private).to.match(/Proc-Type: 4,ENCRYPTED\nDEK-Info: AES-128-CBC/);
+      expect(result.public).to.match(/^ssh-rsa /);
+      expect(result.fingerprint.length > 0);
+      expect(result.randomart.length > 0);
+      done();
+    });
+  });
+
+  it("encrypts using passphrase", (done) => {
+    keygen({ passphrase: "foo bar biz bat" }, (err, result) => {
       expect(expect(err).to.be.null);
       expect(result.private).to.match(/^-----BEGIN RSA PRIVATE KEY-----\n/);
       expect(result.private).to.match(/Proc-Type: 4,ENCRYPTED\nDEK-Info: AES-128-CBC/);
