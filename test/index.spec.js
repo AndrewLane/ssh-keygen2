7c78f3adeb12a9bbd05894f2b84b531c49ba5cdb
Andrew Lane <andrew.w.lane@gmail.com>
2026-09-04 18:39:33 -0400

    Revert "debug"
    This reverts commit 70d2bbdded16454d4f29b6e2f9850d826ba42bc4.

--- test/index.spec.js
+++ test/index.spec.js
@@ -14,7 +14,6 @@ const tmpDir = os.tmpdir();
 describe("basic tests", () => {
   it("generates", (done) => {
     keygen((err, result) => {
-      console.log("error", err);
       expect(err).to.be.null;
       expect(result.private).to.match(/^-----BEGIN (RSA|OPENSSH) PRIVATE KEY-----\n/);
       expect(result.public).to.match(/^ssh-(ed25519|rsa) /);
